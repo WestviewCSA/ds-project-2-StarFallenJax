b6f38dd0216fa0eeee3b7380c508fcdd059df2da
1930158 <1930158@01-18122.pusd.dom>
2025-03-11 13:37:19 -0700

    setting up the main class (p2)

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,20 @@
+import java.io.IOException;
+import java.util.*;
 
 public class p2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("p2");
+		//make sure there are correct arguments
+		if(args.length < 2) {
+			System.out.println("Usage: java p2 --Stack|--Queue|--Opt filename");
+			System.exit(-1);
+		}
+		
+		//check which solver to use
+		boolean useStack = Arrays.asList(args).contains("--Stack");
+		boolean useQueue = Arrays.asList(args).contains("--Queue");
+		boolean useOpt = Arrays.asList(args).contains("--Opt");
+		boolean isCoordinateFormat = Arrays.asList(args).contains("--InCoordinate");
 	}
 
 }
