9fb75aa7ffe76e1b9c6ff67002450f45aa701746
1930158 <1930158@01-18122.pusd.dom>
2025-03-12 12:26:01 -0700

    text parsing

--- P2_first_last/src/Maze.java
+++ P2_first_last/src/Maze.java
@@ -29,6 +29,41 @@ public class Maze {
 	 */
 	private void loadFromText(String filename) throws IOException {
 		//read file and parse the text-based maze representation
+		Scanner scanner = new Scanner(new File(filename));
+		
+		//read the first line for the sauce
+		rows = scanner.nextInt();
+		cols = scanner.nextInt();
+		int numMazes = scanner.nextInt(); //not used yet
+		scanner.nextLine(); //move to the next line
+		
+		//init the maze grid
+		grid = new char[rows][cols];
+		
+		//read the maze line by line
+		for(int i = 0; i < rows; i++) {
+			
+			String line = scanner.nextLine();
+			
+			for(int j = 0; j < cols; j++) {
+				
+				grid[i][j] = line.charAt(j);
+					
+				
+                //store important positions
+                if (grid[i][j] == 'W') {
+                    start = new Position(i, j);
+                } else if (grid[i][j] == '$') {
+                    goal = new Position(i, j);
+                } else if (grid[i][j] == '|') {
+                    walkway = new Position(i, j);
+                }
+				
+				
+			}
+		}
+		
+		scanner.close();
 	}
 	
 	
