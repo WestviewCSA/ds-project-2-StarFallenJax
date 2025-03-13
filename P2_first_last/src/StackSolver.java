edcd836fee47c94727c80bb3e2c694053df162a9
1930158 <1930158@01-18122.pusd.dom>
2025-03-13 13:16:12 -0700

    start of stack solver

--- P2_first_last/src/StackSolver.java
+++ P2_first_last/src/StackSolver.java
@@ -12,21 +12,42 @@ public class StackSolver extends Solver {
      * @return list of positions representing the path, or empty if no path exists
      */
 	@Override
-	public List<Position> findPath(){
-		Stack<Position> stack = new Stack<>();
-		stack.push(maze.start);
-		visited[maze.start.row][maze.start.col] = true;
-		
-		
-		while(!stack.isEmpty()) {
-			Position current = stack.pop();
-			
-			//TODO: implement DFS logic for pathfinding
-			
-		}
-		
-		return path;
-	}
+	public List<Position> findPath() {
+        Stack<List<Position>> stack = new Stack<>(); //stack stores paths
+        List<Position> startPath = new ArrayList<>();
+        startPath.add(maze.start);
+        stack.push(startPath);
+
+        int[][] directions = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} }; // N, S, W, E (directions)
+
+        while (!stack.isEmpty()) {
+            List<Position> path = stack.pop();
+            Position current = path.get(path.size() - 1); //get last position
+
+            //if we reach the goal, return the path
+            if (current.row == maze.goal.row && current.col == maze.goal.col) {
+                return path;
+            }
+
+            //mark position as visited
+            visited[current.row][current.col] = true;
+
+            //explore all 4 directions
+            for (int i = 0; i < 4; i++) {
+                int newRow = current.row + directions[i][0];
+                int newCol = current.col + directions[i][1];
+
+                if (maze.isWalkable(newRow, newCol) && !visited[newRow][newCol]) {
+                    List<Position> newPath = new ArrayList<>(path);
+                    newPath.add(new Position(newRow, newCol));
+                    stack.push(newPath);
+                }
+            }
+        }
+
+        //no path found
+        return new ArrayList<>();
+    }
 	
 	
 }
